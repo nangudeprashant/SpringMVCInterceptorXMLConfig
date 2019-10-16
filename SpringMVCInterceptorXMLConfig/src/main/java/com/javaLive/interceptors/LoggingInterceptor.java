02ecadc7baaef27d6d403eeedccc5e7bf65be70d
Administrator <Administrator@DELL>
2019-10-16 19:48:18 +0530

    Logging mechanism is added.

--- SpringMVCInterceptorXMLConfig/src/main/java/com/javaLive/interceptors/LoggingInterceptor.java
+++ SpringMVCInterceptorXMLConfig/src/main/java/com/javaLive/interceptors/LoggingInterceptor.java
@@ -2,13 +2,18 @@ package com.javaLive.interceptors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 public class LoggingInterceptor implements HandlerInterceptor  {
+	private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class); // SLF4J
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
 		System.out.println("In the logging interceptor which is common to all URLs.");
 		System.out.println("---Before Method Execution. i.e. in the preHandle() method of LoggingInterceptor---");
+		logger.info("In the logging interceptor which is common to all URLs.");
 		return true;
 	}
 	public void postHandle(	HttpServletRequest request, HttpServletResponse response,
--- SpringMVCInterceptorXMLConfig/src/main/java/com/javaLive/interceptors/TransactionInterceptor.java
+++ SpringMVCInterceptorXMLConfig/src/main/java/com/javaLive/interceptors/TransactionInterceptor.java
@@ -2,13 +2,17 @@ package com.javaLive.interceptors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class TransactionInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(TransactionInterceptor.class); // SLF4J
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
 		System.out.println("In the transaction interceptor's preHanlde() method, which is specific to this URL.");
-//		System.out.println("Got request to save data : name:"+request.getParameter("name"));
+		logger.info("In the transaction interceptor's preHanlde() method, which is specific to this URL.");
 		return true;
 	}
 } 
